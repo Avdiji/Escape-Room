1d99893bfbce663cb6fffc793ba055f9e89daafe
avdiji <favdiji@googlemail.com>
2023-05-19 12:43:23 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./commonComponents/layout/Layout";
 import BluePuzzleButton from "./commonComponents/dresser/blueDrawer/BluePuzzleButton";
 import { useState } from "react";
-import HintMarker from "./commonComponents/hintMarker/HintMarker";
 import Room from "./pages/Room";
 
 export default function App() {
@@ -12,8 +11,6 @@ export default function App() {
   return (
     <Router>
       <Layout setFlashlight={setFlashlight} flashlightOn={flashlightOn}>
-        {/* <BluePuzzleButton getArrowCombination={getArrowCombination} x="50px" y="100px"/> */}
-        {/* <HintMarker x="50px" y="150px"/> */}
         <Room title="Blaues Zimmer" src="/images/blueRoom/Blau_Sc1_Hellxcf.png">
           <BluePuzzleButton getArrowCombination={getArrowCombination} x="15vw" y="-25vw"/>
         </Room>
--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,47 +1,40 @@
+import { Link } from "react-router-dom";
 import CardEffect from "../commonComponents/cardEffect/CardEffect";
 import BluePuzzleButton from "../commonComponents/dresser/blueDrawer/BluePuzzleButton";
 import classes from "./Room.module.css";
 import { useState } from "react";
 
 export default function Room(props) {
-  var [getArrowCombination] = useState([0, 0, 0, 3, 0]);
   const imgSource = process.env.PUBLIC_URL + props.src;
 
   return (
     <div className={classes.room}>
-        <h1>{props.title}</h1>
-      <img
-        className={classes.arrowImg}
-        src={process.env.PUBLIC_URL + "/images/roomArrows/arrowUp.png"}
-        alt=""
-      />
+      <h1>{props.title}</h1>
+
+      <Link>
+        <img className={classes.arrowImg} src={process.env.PUBLIC_URL + "/images/roomArrows/arrowUp.png"} alt="" />
+      </Link>
 
       <div className={classes.scrollContainer}>
         <div className={classes.arrowImageContainer}>
-          <img
-            className={classes.arrowImg}
-            src={process.env.PUBLIC_URL + "/images/roomArrows/arrowLeft.png"}
-            alt=""
-          />
+          <Link>
+            <img className={classes.arrowImg} src={process.env.PUBLIC_URL + "/images/roomArrows/arrowLeft.png"} alt="" />
+          </Link>
 
           <div className={classes.roomContainer}>
             <img className={classes.roomImg} src={imgSource} alt="" />
             {props.children}
           </div>
 
-          <img
-            className={classes.arrowImg}
-            src={process.env.PUBLIC_URL + "/images/roomArrows/arrowRight.png"}
-            alt=""
-          />
+          <Link>
+            <img className={classes.arrowImg} src={process.env.PUBLIC_URL + "/images/roomArrows/arrowRight.png"} alt="" />
+          </Link>
         </div>
       </div>
 
-      <img
-        className={classes.arrowImg}
-        src={process.env.PUBLIC_URL + "/images/roomArrows/arrowDown.png"}
-        alt=""
-      />
+      <Link>
+        <img className={classes.arrowImg} src={process.env.PUBLIC_URL + "/images/roomArrows/arrowDown.png"} alt="" />
+      </Link>
     </div>
   );
 }
